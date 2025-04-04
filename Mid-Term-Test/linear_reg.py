bc2fa536f17fa288b2a13343adfd7631092a0d0a
Bug-pirate <patiladitya914@gmail.com>
2025-04-04 11:06:40 +0530

    Mid Term Exam

--- Mid-Term-Test/linear_reg.py
+++ Mid-Term-Test/linear_reg.py
@@ -1,12 +1,10 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
-from pandas.core.common import random_state
 from sklearn.linear_model import LinearRegression
-import kagglehub  
-
+import pandas as pd
+import seaborn as sns
+from pandas.core.common import random_state
 
 
 df_sal = pd.read_csv('Salary_Data.csv')
@@ -19,17 +17,18 @@ sns.distplot(df_sal['Salary'])
 
 plt.show()
  
-plt.scatter(df_sal['YearsExperience'], df_sal['Salary'], color = 'lightcoral')
+plt.scatter(df_sal['YearsExperience'], df_sal['Salary'], color = 'red')
 plt.title('Salary vs Experience')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.box(False)
 plt.show()
+
  
 X = df_sal.iloc[:, :1]  
 y = df_sal.iloc[:, 1:]  
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
@@ -37,9 +36,9 @@ regressor.fit(X_train, y_train)
 y_pred_test = regressor.predict(X_test)     
 y_pred_train = regressor.predict(X_train)   
 
-plt.scatter(X_train, y_train, color = 'lightcoral')
+plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, y_pred_train, color = 'firebrick')
-plt.title('Salary vs Experience (Training Set)')
+plt.title('Salary vs Experience - Training Set')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.legend(['X_train/Pred(y_test)', 'X_train/y_train'], title = 'Sal/Exp', loc='best', facecolor='white')
